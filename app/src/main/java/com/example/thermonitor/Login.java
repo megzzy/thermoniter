4fa9a97c78c83bde12e2df4e16680723a1930467
megzyy <ahmod.magdy@gmail.com>
2019-02-28 22:50:32 +0200

    thermonitor

--- app/src/main/java/com/example/thermonitor/Login.java
+++ app/src/main/java/com/example/thermonitor/Login.java
@@ -2,9 +2,9 @@ package com.example.thermonitor;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +17,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import org.w3c.dom.Text;
-
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     private Button         buttonLogin;
@@ -28,6 +26,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     private ProgressDialog progressDialog;
     private FirebaseAuth   firebaseAuth;
     private TextView       textForgotPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,6 +112,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 }
 
             }
+
         }
 
 
--- app/src/main/java/com/example/thermonitor/Profile.java
+++ app/src/main/java/com/example/thermonitor/Profile.java
@@ -1,9 +1,14 @@
 package com.example.thermonitor;
 
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -12,85 +17,125 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+//
+//
 public class Profile extends AppCompatActivity implements View.OnClickListener {
+    //
 
-    int[] images   = { R.drawable.c , R.drawable.php , R.drawable.java , R.drawable.javascript , R.drawable.cc, R.drawable.python , R.drawable.sql };
-    String[] names = { "c" , "php" , "java" , "java script" , "c++" , "Python" , "SQL" };
+    int images=R.drawable.esp;
 
+//
     private FirebaseAuth firebaseAuth;
-    private TextView     textViewUserE;
-    private Button       buttonLogOut;
+    private TextView textViewUserE;
+    private Button buttonLogOut;
+    private WifiManager wifiManager;
+    private List<ScanResult> results;
+    private Button buttonSCN;
+    private ArrayList<String> names = new ArrayList<>();
+    private ArrayList<String> mac = new ArrayList<>();
+    private int size = 0;
+    private ListView listView;
+    private CustomAdapter adapter;
 
     @Override
+//
     protected void onCreate(Bundle savedInstanceState) {
+//
         super.onCreate(savedInstanceState);
+//
         setContentView(R.layout.activity_profile);
+        textViewUserE = (TextView) findViewById(R.id.textViewUserE);
 
-        textViewUserE = (TextView)findViewById(R.id.textViewUserE);
-        buttonLogOut  = (Button)findViewById(R.id.buttonLogOut);
+        buttonLogOut = (Button) findViewById(R.id.buttonLogOut);
 
         buttonLogOut.setOnClickListener(this);
 
-        firebaseAuth  = FirebaseAuth.getInstance();
-        if (firebaseAuth.getCurrentUser()== null){
-            finish();
-            startActivity(new Intent(getApplicationContext(),Login.class));
-        }
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), Login.class));
+        }
         FirebaseUser user = firebaseAuth.getCurrentUser();
         textViewUserE.setText("Welcome \n" + user.getEmail());
-
-
-        ListView listView = (ListView)findViewById(R.id.ListView);
-
-        CustomAdapter customAdapter = new CustomAdapter();
-        listView.setAdapter(customAdapter);
+//
+        listView = findViewById(R.id.wifiList);
+//
+         adapter = new CustomAdapter();
+//
+        listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position==0){
-                    Intent intent0=new Intent(view.getContext(),C.class);
-                    startActivity(intent0);
-                }
-                if (position==1){
-                    Intent intent1=new Intent(view.getContext(),PHP.class);
-                    startActivity(intent1);
-                }
-                if (position==2){
-                    Intent intent2=new Intent(view.getContext(),Java.class);
-                    startActivity(intent2);
-                }
-                if (position==3){
-                    Intent intent3=new Intent(view.getContext(),JavaScript.class);
-                    startActivity(intent3);
-                }
-                if (position==4){
-                    Intent intent4=new Intent(view.getContext(),C_plus_plus.class);
-                    startActivity(intent4);
-                }
-                if (position==5){
-                    Intent intent5=new Intent(view.getContext(),Python.class);
-                    startActivity(intent5);
-                }
-                if (position==6){
-                    Intent intent6=new Intent(view.getContext(),SQL.class);
-                    startActivity(intent6);
-                }
+                Intent intent0=new Intent(view.getContext(),tempread.class);
+                startActivity(intent0);
 
             }
         });
 
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(this, "Wifi is disabled ... Please turn it on", Toast.LENGTH_SHORT).show();
+            wifiManager.setWifiEnabled(true);
+        }
+        buttonSCN = (Button) findViewById(R.id.buttonSCN);
+        buttonSCN.setOnClickListener(this);
+
+        listView.setAdapter(adapter);
+        scanWifi();
+
+
+
     }
 
-    class CustomAdapter extends BaseAdapter{
+    private void scanWifi() {
+        names.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(wifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this, "Scanning for WIFI ...", Toast.LENGTH_SHORT).show();
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+            for (ScanResult scanResult : results) {
+                if(scanResult.SSID.equals("ESP8266")) {
+                    names.add(scanResult.SSID);
+                    mac.add((scanResult.BSSID));
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonLogOut) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, Login.class));
+        }
+        if (v == buttonSCN)
+            scanWifi();
+
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
 
         @Override
         public int getCount() {
-            return images.length ;
+            return names.size();
         }
 
         @Override
@@ -105,25 +150,15 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-
-            convertView = getLayoutInflater().inflate(R.layout.customlayout,null);
-            ImageView imageView=(ImageView)convertView.findViewById(R.id.imageView);
-            TextView textView=(TextView)convertView.findViewById(R.id.textView);
-            imageView.setImageResource(images[position]);
-            textView.setText(names[position]);
+            convertView = getLayoutInflater().inflate(R.layout.customlayout, null);
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+            TextView textView   = (TextView) convertView.findViewById(R.id.textView);
+            TextView textView4  = (TextView) convertView.findViewById(R.id.textView4);
+            imageView.setImageResource(images);
+            textView.setText(names.get(position));
+            textView4.setText(mac.get(position));
             return convertView;
         }
     }
 
-    @Override
-    public void onClick(View v) {
-
-        if (v == buttonLogOut){
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(this , Login.class));
-        }
-
-
-    }
 }
